37abfac427a99fe2edfefdee18d47378bff5fdd5
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-04-20 22:23:31 +0200

    feat(server): add spring websockets

--- server/src/main/java/nz/examplewastaken/openatc/OpenATC.java
+++ server/src/main/java/nz/examplewastaken/openatc/OpenATC.java
@@ -2,6 +2,6 @@ package main.java.nz.examplewastaken.openatc;
 
 public class OpenATC {
     public static void main(String[] args) {
-        System.out.println("OpenATC running");
+        System.out.println("OpenATC!!! Look at that!");
     }
 }
