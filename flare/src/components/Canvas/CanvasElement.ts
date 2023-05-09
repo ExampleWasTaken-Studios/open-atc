d3790491e164e41a4aa6099b899424d812aa4575
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-05-09 02:53:07 +0200

    chore: remove flare (#6)
    * fix(flare): update package name in package-lock.json
    
    Signed-off-by: ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
    
    * ci(flare): add flare publish workflow script
    
    Signed-off-by: ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
    
    * chore(flare): remove flare
    
    Signed-off-by: ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>

--- flare/src/components/Canvas/CanvasElement.ts
+++
@@ -1,7 +0,0 @@
-import { Interactable } from '../../interactable/Interactable';
-
-export interface CanvasElementProps extends Interactable {
-  x: number;
-  y: number;
-  z: number;
-}
--- flare/src/interactable/Interactable.ts
+++
@@ -1,7 +0,0 @@
-import { MouseEvent } from 'react';
-
-export interface Interactable {
-  onMouseMove?: (event: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => void;
-  onMouseUp?: (event: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => void;
-  onMouseDown?: (event: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => void;
-}
