3ec15f42d24bbdaa52279d291d87b15c5ebb5d27
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-08-09 11:24:40 +0200

    add disabled state to default button

--- client/src/renderer/src/components/ServerControlPanel/ServerControlPanel.tsx
+++ client/src/renderer/src/components/ServerControlPanel/ServerControlPanel.tsx
@@ -1,5 +1,6 @@
 import { WebSocketManager } from '../../network/WebSocketManager';
 import { DefaultButton } from '../util/DefaultButton';
+import { useState } from 'react';
 
 interface ServerControlPanelProps {
   webSocketManager: WebSocketManager;
@@ -12,10 +13,20 @@ export const ServerControlPanel = ({
 }: ServerControlPanelProps) => {
   const css = className + 'bg-gray-300 p-2';
 
+  const [buttonDisabledState, setButtonDisabledState] = useState(false);
+
   return (
     <div className={css}>
+      <h3 className="font-bold">SRVR CNTRL PNL</h3>
       <p>Status: {webSocketManager.getReadyStateString()}</p>
-      <DefaultButton onClick={() => console.log('Connection toggle clicked')}>
+      <DefaultButton
+        onClick={() => {
+          console.log('Connection toggle clicked.');
+          setButtonDisabledState(true);
+          setTimeout(() => setButtonDisabledState(false), 3000);
+        }}
+        disabled={buttonDisabledState}
+      >
         {webSocketManager.getReadyState() == 1 ? 'Disconnect' : 'Connect'}
       </DefaultButton>
     </div>
--- client/src/renderer/src/components/util/DefaultButton.tsx
+++ client/src/renderer/src/components/util/DefaultButton.tsx
@@ -1,22 +1,25 @@
 interface DefaultButtonProps {
   onClick: () => void;
+  disabled?: boolean;
   children: string;
   className?: string;
 }
 
+const styling =
+  'bg-gray-400 border-blue-500 border p-2 cursor-pointer select-none text-center active:scale-[.95]';
+
+const disabledStyling =
+  'bg-gray-400 border-gray-500 border p-2 cursor-pointer select-none text-center text-gray-500';
+
 export const DefaultButton = ({
   onClick,
+  disabled,
   children,
-  className,
 }: DefaultButtonProps) => {
   return (
     <div
-      className={
-        className
-          ? className
-          : 'bg-gray-400 border-blue-500 border p-2 cursor-pointer select-none text-center active:scale-[.95]'
-      }
-      onClick={onClick}
+      className={disabled ? disabledStyling : styling}
+      onClick={() => !disabled && onClick()}
     >
       {children}
     </div>
